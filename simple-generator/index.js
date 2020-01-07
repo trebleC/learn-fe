62ea065133b83712abde9060f6eec48db61d1c5b
luoquanquan <luoquanquan@baidu.com>
2020-01-07 13:09:07 +0800

    使用 generator 实现迭代

--- simple-generator/index.js
+++ simple-generator/index.js
@@ -18,3 +18,24 @@ const arr = [...obj]
 
 console.log(Array.isArray(arr), arr)
 console.log(Array.isArray(obj), obj)
+
+const fns = () => {
+    const obj = {
+        0: 1,
+        1: 2,
+        2: 3,
+        length: 3,
+        * [Symbol.iterator]() {
+            let index = 0
+            while (index !== this.length) {
+                yield this[index++]
+            }
+        }
+    }
+    const arr = [...obj]
+
+    console.log(Array.isArray(arr), arr)
+    console.log(Array.isArray(obj), obj)
+}
+
+fns()

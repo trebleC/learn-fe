0f74eed7a43f6f70271be57c9ea6f30b301520be
luoquanquan <luoquanquan@baidu.com>
2020-01-12 23:47:32 +0800

    使用 pipe 实现读写流的配合

--- node-fs/read+write-stream.js
+++ node-fs/read+write-stream.js
@@ -8,18 +8,21 @@ const ws = fs.createWriteStream('./files/write-demo-file', {
     highWaterMark: 1
 })
 
-rs.on('data', chunk => {
-    const canDeal = ws.write(chunk)
-    // 尝试写入当前可读流的内容的时候发现撑到了, 暂停继续读取
-    if (!canDeal) rs.pause()
-})
+// rs.on('data', chunk => {
+//     const canDeal = ws.write(chunk)
+//     // 尝试写入当前可读流的内容的时候发现撑到了, 暂停继续读取
+//     if (!canDeal) rs.pause()
+// })
 
-// 可写流抽干(也就是写完了), 启动可读流继续读取
-ws.on('drain', () => {
-    rs.resume()
-})
+// // 可写流抽干(也就是写完了), 启动可读流继续读取
+// ws.on('drain', () => {
+//     rs.resume()
+// })
 
-// 可读流读取完毕, 尝试关闭可写流
-rs.on('end', () => {
-    ws.end('文件读写完毕~')
-})
+// // 可读流读取完毕, 尝试关闭可写流
+// rs.on('end', () => {
+//     ws.end('文件读写完毕~')
+// })
+
+// 使用 pipe 把可读流导入到可写流
+rs.pipe(ws)

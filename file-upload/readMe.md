ff99e4114e78cd0913a143ba93398f0bb27d15a1
luoquanquan <luoquanquan@baidu.com>
2020-01-14 19:07:57 +0800

    step

---
+++ file-upload/readMe.md
@@ -0,0 +1,7 @@
+# 文件上传
+
+## ps: 当前目录下所有的 demo 运行都依赖全局安装 nodemon
+
+```shell
+npm i nodemon -g
+```
---
+++ file-upload/split-upload/utils/db.js
@@ -0,0 +1,17 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({
+    files: []
+}).write()
+
+exports.getFile = md5 => {
+
+}
+
+exports.setFile = file => {
+
+}
+

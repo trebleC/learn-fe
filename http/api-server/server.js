3f9c822a9bad5d484eef87b426d3a90704b5b7f9
luoquanquan <luoquanquan@baidu.com>
2020-01-18 23:31:40 +0800

    实现同域的 get 和 put 两种请求

---
+++ http/api-server/server.js
@@ -0,0 +1,69 @@
+const url = require('url')
+const http = require('http')
+const { join } = require('path')
+const fs = require('mz/fs')
+const mime = require('mime')
+
+const basePath = join(__dirname)
+
+class Server {
+    constructor(port) {
+        this.port = port || 3333
+    }
+
+    async handleRequest(request, response) {
+        const { pathname } = url.parse(request.url, true)
+        let absPath = join(basePath, pathname)
+
+        let { method } = request
+        method = method.toLowerCase()
+
+        switch (pathname) {
+            case '/user':
+                    if (method === 'get') {
+                        response.setHeader('Content-Type', 'application/json')
+                        response.end(JSON.stringify({name: 'quanquan', method: 'get'}))
+                    }
+
+                    if (method === 'put') {
+                        response.setHeader('Content-Type', 'application/json')
+                        response.end(JSON.stringify({name: 'quanquan', method: 'put'}))
+                    }
+                break;
+            default:
+                break;
+        }
+        try {
+            const statusObj = await fs.stat(absPath)
+
+            if (statusObj.isDirectory()) {
+                absPath = join(absPath, 'index.html')
+            }
+
+            await fs.access(absPath)
+
+            this.renderFile(absPath, response)
+        } catch (error) {
+            this.renderError(response)
+        }
+    }
+
+    renderFile(absPath, response) {
+        response.setHeader('Content-Type', mime.getType(absPath))
+        fs.createReadStream(absPath).pipe(response)
+    }
+
+    renderError(response) {
+        response.statusCode = 404
+        response.end('Not found~')
+    }
+
+    start() {
+        const server = http.createServer(this.handleRequest.bind(this))
+        server.listen(this.port, () => {
+            console.log(`the server is running on ${this.port}`)
+        })
+    }
+}
+
+new Server().start()

5b9093223a4f45116ffa8af61ba2c0848a70d04c
luoquanquan <luoquanquan@baidu.com>
2020-01-22 11:50:55 +0800

    实现 Cookie 的签名认证

--- simple-cookie/server.js
+++ simple-cookie/server.js
@@ -1,20 +1,42 @@
 const http = require('http')
 const querystring = require('querystring')
 const url = require('url')
+const crypto = require('crypto')
+
+const salt = 'quan'
 const PORT = 3333
+
+const sign = v => crypto.createHmac('sha256', salt)
+    .update(v).digest('base64')
+    // 由于浏览器请求携带的 Cookie 中存在的 + 会被转为空格, 所以把 base64 纯在的 + 干掉
+    .replace(/\+/g, '')
 
 http.createServer((request, response) => {
     const {pathname} = url.parse(request.url)
 
     const cookieArr = []
     response.setCookie = (k, v, opt) => {
-        cookieArr.push(`${k}=${v}`)
+        cookieArr.push(`${k}=${v}${opt.sign ? '.' + sign(v) : ''}`)
         for (let key in opt) {
+            if (key === 'sign') {
+                continue
+            }
             cookieArr[cookieArr.length - 1] += `; ${key}=${opt[key]}`
         }
 
         // 利用多次设置 Set-Cookie 响应头以最后一次为准, 设置所有 cookie
         response.setHeader('Set-Cookie', cookieArr)
+    }
+
+    request.getSignCookie = key => {
+        const {cookie = {}} = request.headers
+        const {[key]: value = ''} = querystring.parse(cookie, '; ')
+
+        const [v, cipher] = value.split('.')
+        if (sign(v) === cipher) {
+            return v
+        }
+        return ''
     }
 
     request.getCookie = key => {
@@ -25,13 +47,14 @@ http.createServer((request, response) => {
 
     switch (pathname) {
         case '/get': case '/get/2': case '/get2':
-            response.end(request.getCookie('name'))
+            response.end(request.getSignCookie('name'))
             break
         case '/set':
 
             response.setCookie('name', 'quanquan', {
                 domain: 'quanquan.com',
-                path: '/'
+                path: '/',
+                sign: true
             })
 
             response.setCookie('age', '18', {

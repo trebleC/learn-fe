3aa794fffc21dc8a9b67e810d58f9aac4b96afeb
luoquanquan <luoquanquan@baidu.com>
2020-01-23 16:48:35 +0800

    先创建一个简单的小项目

---
+++ pwa/server.js
@@ -0,0 +1,26 @@
+const Koa = require('koa')
+const KoaRouter = require('koa-router')
+const koaStatic = require('koa-static')
+const path = require('path')
+
+const conf = {
+    PORT: 3333
+}
+
+const app = new Koa()
+const router = new KoaRouter()
+
+router.get('/api/getImage', ctx => {
+    const ret = []
+    while (ret.length < 99) {
+        const idx = Math.ceil(Math.random() * 9)
+        ret.push(`http://img.blog.niubishanshan.top/${idx}.jpeg`)
+    }
+    ctx.body = ret
+})
+
+app.use(router.routes(), router.allowedMethods())
+app.use(koaStatic(path.resolve(__dirname, 'public')))
+app.listen(conf.PORT, () => {
+    console.log(`the server is runing at ${conf.PORT}`)
+})

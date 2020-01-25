942e09ebd7e0b2aeb8f5f9dbdfa894b41252ff82
luoquanquan <luoquanquan@baidu.com>
2020-01-25 20:58:56 +0800

    添加 vue-router

--- pwa/pwa-vue/src/App.vue
+++ pwa/pwa-vue/src/App.vue
@@ -1,32 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld :msg="announce" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  data() {
-    return {
-      announce: 'Welcome to Your Vue.js App'
-    }
-  },
-  components: {
-    HelloWorld
-  },
-  mounted() {
-    fetch('http://127.0.0.1:3333/pwa-vue-demo')
-      .then(resp => resp.json())
-      .then(resp => {
-        this.announce = resp.announce
-      })
-  },
-}
-</script>
 
 <style>
 #app {
@@ -35,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>

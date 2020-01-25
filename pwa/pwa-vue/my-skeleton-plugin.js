6fcfc8e24791ef9421bff2a91fd9d83aebb5ee63
luoquanquan <luoquanquan@baidu.com>
2020-01-25 22:18:34 +0800

    自己实现了一个 webpack Skeleton 极简版本

--- pwa/pwa-vue/my-skeleton-plugin.js
+++ pwa/pwa-vue/my-skeleton-plugin.js
@@ -2,8 +2,21 @@ class MySkeletonPlugin {
     apply(compiler) {
         compiler.hooks.compilation.tap('html-webpack-plugin-before-html-generation', function (compilation) {
             compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('MySkeletonPlugin', function (htmlPluginData, callback) {
-                console.log(`当前时间 ${Date.now()}: debug 的数据是 htmlPluginData: `, htmlPluginData)
-                htmlPluginData.html = 'hello world'
+                htmlPluginData.html = htmlPluginData.html.replace(/<div id="app"><\/div>/,
+                `<div id=app>
+                    <div id=Skeleton style="display: none;">
+                        <h1>这是首页</h1>(真实情况下回放一张 base64 的图片~)
+                    </div>
+                    <div id=SkeletonAbout style="display: none;">
+                        <h1>这是关于页面</h1>(真实情况下回放一张 base64 的图片~)
+                    </div>
+                    <script>const { hash } = location
+                        let id = 'SkeletonAbout'
+                        if (hash === '#/') {
+                            id = 'Skeleton'
+                        }
+                        document.getElementById(id).style.display = 'block'</script>
+                </div>`)
                 callback()
             })
         });

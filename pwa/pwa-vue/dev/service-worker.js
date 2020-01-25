00978db3557d2ddc6ff3b1777b131c720c41411a
luoquanquan <luoquanquan@baidu.com>
2020-01-25 12:22:07 +0800

    使用自定义配置设置 pwa

---
+++ pwa/pwa-vue/dev/service-worker.js
@@ -0,0 +1,16 @@
+// 初始化缓存名称
+workbox.core.setCacheNameDetails({prefix: "pwa-vue"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+// 缓存列表, 可以配置自定义的缓存
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+
+// 执行缓存操作
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+console.log('自定义的缓存配置文件~')

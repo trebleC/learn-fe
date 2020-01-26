83f917da3bd24443f08d83b4c3f3df87022ce7d1
luoquanquan <luoquanquan@baidu.com>
2020-01-26 10:13:05 +0800

    将模板提取到单独文件

--- vue-ssr/vue-server-render/server.js
+++ vue-ssr/vue-server-render/server.js
@@ -1,11 +1,16 @@
+const fs = require('fs')
+const path = require('path')
 const Koa = require('koa')
 const KoaRouter = require('koa-router')
 const Vue = require('vue')
-const vueServerRenderer = require('vue-server-renderer')
+const { createRenderer } = require('vue-server-renderer')
 
 const conf = {
     PORT: 3333
 }
+
+const template = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8')
+const renderer = createRenderer({ template })
 
 const app = new Koa()
 const router = new KoaRouter()
@@ -15,28 +20,14 @@ router.get('/', ctx => {
         template: '<div>hello world~</div>'
     })
 
-    vueServerRenderer.createRenderer()
-        .renderToString(vm, (err, htmlStr) => {
-            if (err) {
-                ctx.statusCode = 502
-                ctx.body = 'Server Error'
-                return;
-            }
-            ctx.body = `
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                    <title>vue-server-renderer</title>
-                </head>
-                <body>
-                    ${htmlStr}
-                </body>
-                </html>
-            `
-        })
+    renderer.renderToString(vm, (err, htmlStr) => {
+        if (err) {
+            ctx.statusCode = 502
+            ctx.body = 'Server Error'
+            return;
+        }
+        ctx.body = htmlStr
+    })
 })
 
 app.use(router.routes(), router.allowedMethods())

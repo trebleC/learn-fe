4e6d73ee8ee665a34316497ea3363bbca81268d2
luoquanquan <luoquanquan@baidu.com>
2020-01-26 22:31:45 +0800

    服务端渲染这就完了

---
+++ vue-ssr/simple-vue-env/server.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+const Koa = require('koa')
+const KoaRouter = require('koa-router')
+const { createBundleRenderer } = require('vue-server-renderer')
+
+
+const conf = {
+    PORT: 3333
+}
+
+const serverBundle = fs.readFileSync(path.resolve(__dirname, './dist/server.bundle.js'), 'utf8')
+const template = fs.readFileSync(path.resolve(__dirname, './dist/index.ssr.html'), 'utf8')
+const renderer = createBundleRenderer(serverBundle, { template })
+
+const app = new Koa()
+const router = new KoaRouter()
+
+router.get('/', async ctx => {
+    try {
+        const htmlStr = await renderer.renderToString(ctx)
+        ctx.body = htmlStr
+    } catch (error) {
+        ctx.status = 502
+        ctx.body = 'Server Error'
+    }
+})
+
+app.use(router.routes(), router.allowedMethods())
+app.listen(conf.PORT, () => {
+    console.log(`the server is listen on ${conf.PORT}`)
+})

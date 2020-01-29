1007852b4d96f4f94ca2b186b4a98f7ee5472836
luoquanquan <luoquanquan@baidu.com>
2020-01-30 00:17:58 +0800

    User === conn.model('User') => true

--- mongo-learn/mongoose-learn/index.js
+++ mongo-learn/mongoose-learn/index.js
@@ -20,6 +20,7 @@ const userSchema = new mongoose.Schema({
 
 const User = conn.model('User', userSchema)
 
+console.log(User === conn.model('User'))
 ;(async () => {
     const ret = await User.create({
         name: 'quanquan',

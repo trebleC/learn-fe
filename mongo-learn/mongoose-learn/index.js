43fefcbec25bfef23852426931117a9a557f4e6f
luoquanquan <luoquanquan@baidu.com>
2020-01-29 23:36:09 +0800

    成功使用 mongoose 连接数据库并创建了一条记录

---
+++ mongo-learn/mongoose-learn/index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const conn = mongoose.createConnection('mongodb://localhost/users', {
+    useNewUrlParser: true
+})
+
+conn.on('open', () => {
+    console.log(`当前时间 ${Date.now()}: 代码走到了这里 连接成功`)
+})
+
+conn.on('error', () => {
+    console.log(`当前时间 ${Date.now()}: 代码走到了这里 连接失败`)
+})
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    sex: String
+})
+
+const User = conn.model('User', userSchema)
+
+;(async () => {
+    const ret = await User.create({
+        name: 'quanquan',
+        age: 28,
+        sex: 'male'
+    })
+    console.log(`当前时间 ${Date.now()}: debug 的数据是 ret: `, ret)
+})()
+

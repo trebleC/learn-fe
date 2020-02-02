1f04c7391ca27c7c1ac6d9a0f09551cf19925614
luoquanquan <luoquanquan@baidu.com>
2020-02-02 20:13:20 +0800

    第三种方法: 把数组中的元素作为对象的属性名, 利用对象的属性名不会重复的特性实现数组去重

--- basic-js/array-unique.js
+++ basic-js/array-unique.js
@@ -44,3 +44,26 @@ const unique2 = arr => {
 }
 
 renderResult(origin, unique2)
+
+// 当数字作为 object 的属性时会被转为字符串, 所以这个方法只能用于
+// 单一类型元素的素组, 不能既包含数字又包含其他类型
+const unique3 = arr => {
+    const obj = {}
+    const ret = []
+
+    for (let i = 0, len = arr.length; i < len; i++) {
+        obj[arr[i]] = null
+    }
+
+    for (const j in obj) {
+        // 数字类型的数组去重需要强转会数字
+        ret.push(Number(j))
+        // 字符串类型的数组
+        // ret.push(j)
+    }
+
+    return ret
+}
+
+
+renderResult(origin, unique3)

42cb31376aeef30c93f4c72f9046854957326552
luoquanquan <luoquanquan@baidu.com>
2020-02-08 23:11:30 +0800

    抽离创建随机数组的方法

---
+++ basic-js/array-sort/bubbleSort.js
@@ -0,0 +1,5 @@
+const {createRandomArr} = require('./utils')
+const randomArr = createRandomArr(1e3, 0, 1e5)
+
+
+
---
+++ basic-js/array-sort/utils.js
@@ -0,0 +1,18 @@
+const randomNum = (min = 0, max = min) => {
+    if (max === min) {
+        return parseInt(Math.random() * (min + 1))
+    } else {
+        return parseInt(Math.random() * (max - min + 1) + min)
+    }
+}
+
+exports.createRandomArr = (len, min, max) => {
+    const ret = new Array(len)
+
+    for (let i = 0; i < len; i++) {
+        ret[i] = randomNum(min, max)
+    }
+
+    return ret
+}
+

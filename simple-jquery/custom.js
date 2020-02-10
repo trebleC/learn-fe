dbdc766db333d3c71cdda85d9b95a281761d004b
luoquanquan <luoquanquan@baidu.com>
2020-02-10 09:15:46 +0800

    我们 $() 创建的东西应该是 jQuery.fn.init 的实例而非 jQuery 的实例, 通过共享原型的方式实现 jQuery.fn.init 创建的实例也能使用 jQ 原型上的方法

--- simple-jquery/custom.js
+++ simple-jquery/custom.js
@@ -1,8 +1,18 @@
 ((root, undefined) => {
-    // 这样写明显会出现死循环
     const jQuery = function () {
-        return new jQuery()
+        return new jQuery.fn.init()
     }
 
+    jQuery.fn = jQuery.prototype = {
+        init: function() {
+            this.name = 'quanquan'
+            return this
+        },
+        css() {
+            console.log(`当前时间 ${Date.now()}: 代码走到了这里 我是 Css method`)
+        }
+    }
+
+    jQuery.fn.init.prototype = jQuery.fn
     root.$ = root.jQuery = jQuery
 })(window)

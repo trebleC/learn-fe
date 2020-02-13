3f734ccbf95a6f649a712f88fe57efff40a0f2b2
luoquanquan <luoquanquan@baidu.com>
2020-02-13 22:32:38 +0800

    换用大的随机数组

--- basic-js/array-sort/insertionSort.js
+++ basic-js/array-sort/insertionSort.js
@@ -1,5 +1,5 @@
 const { createRandomArr } = require('./utils')
-const randomArr = [9, 10, 0, 2, 1]
+const randomArr = createRandomArr(1e4, 0, 1e8)
 
 const insertionSort = array => {
     const len = array.length
@@ -8,7 +8,6 @@ const insertionSort = array => {
     for (outerIndex = 1; outerIndex < len; outerIndex++) {
         temp = array[outerIndex]  // 待排元素
         innerIndex = outerIndex - 1 // 已经排好的队伍
-        console.log(`当前排序好的队伍 ${array.slice(0, outerIndex)}`)
         // 拿当前的待排元素到已经排好的队伍里边找他合适的位置
         while (innerIndex >= 0 && array[innerIndex] > temp) {
             array[innerIndex + 1] = array[innerIndex]
@@ -23,5 +22,4 @@ const insertionSortArray = randomArr.slice()
 console.time('insertionSort')
 insertionSort(insertionSortArray)
 console.timeEnd('insertionSort')
-console.log(insertionSortArray)
 

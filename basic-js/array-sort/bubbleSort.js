6defb26867e3cc61b0ddf3f23d3079fae186b324
luoquanquan <luoquanquan@baidu.com>
2020-02-13 14:45:27 +0800

    升级后的冒泡排序算法

--- basic-js/array-sort/bubbleSort.js
+++ basic-js/array-sort/bubbleSort.js
@@ -20,4 +20,24 @@ bubbleSort(bubbleSortArray)
 console.timeEnd('bubbleSort')
 console.log(bubbleSortArray)
 
+const modifiedBubbleSort = array => {
+    const len = array.length
+
+    for (let outerIndex = 0; outerIndex < len; outerIndex++) {
+        for (let innerIndex = 0; innerIndex < len - 1 - outerIndex; innerIndex++) {
+            console.log(array[innerIndex], array[innerIndex + 1])
+            if (array[innerIndex] > array[innerIndex + 1]) {
+                swap(array, innerIndex, innerIndex + 1)
+            }
+        }
+        console.log(array)
+    }
+}
+
+const modifiedBubbleSortArray = randomArr.slice()
+console.time('modifiedBubbleSort')
+modifiedBubbleSort(modifiedBubbleSortArray)
+console.timeEnd('modifiedBubbleSort')
+console.log(modifiedBubbleSortArray)
+
 

10e8b6dcfac4b6ec43235ce65d31bd84dfe4c8b8
luoquanquan <luoquanquan@baidu.com>
2020-02-15 12:36:08 +0800

    实现反射型 XSS

--- web-security/xss/server.js
+++ web-security/xss/server.js
@@ -9,6 +9,22 @@ const app = new Koa()
 const router = new KoaRouter()
 app.use(KoaStatic(__dirname))
 
+router.get('/reflect.html', (ctx) => {
+    const {userName} = ctx.query
+    ctx.body = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>反射型 XSS</title>
+        </head>
+        <body>
+            <h1>Hi, <span id="user-name">${userName}</span></h1>
+        </body>
+        </html>
+    `
+})
 app.use(router.routes(), router.allowedMethods())
 app.listen(conf.PORT, () => {
     console.log(`the server is listening on ${conf.PORT}`)

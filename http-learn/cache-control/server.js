2069e3a48355489bfc017f4e538f52bd22207a60
luoquanquan <luoquanquan@baidu.com>
2020-03-04 16:38:17 +0800

    没有设置缓存响应头之前, 每次刷新都会请求所有文件
    /
    /script.js
    /favicon.ico

---
+++ http-learn/cache-control/server.js
@@ -0,0 +1,39 @@
+const http = require('http')
+const conf = {
+    PORT: 3333
+}
+
+http.createServer((request, response) => {
+    const {url} = request
+    console.log(url)
+    if (url === '/') {
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+
+        response.end(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>cache-control</title>
+            </head>
+            <body>
+
+                <script src="/script.js"></script>
+            </body>
+            </html>
+        `)
+    }
+
+    if (url === '/script.js') {
+        response.writeHead(200, {
+            'Content-Type': 'text/javascritp'
+        })
+
+        response.end('console.log("script loaded~")')
+    }
+}).listen(conf.PORT, () => {
+    console.log(`the server is running on ${conf.PORT}`)
+})

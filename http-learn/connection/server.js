f353e0efa2c2291cab1095bdbf918141a66a83d4
luoquanquan <luoquanquan@baidu.com>
2020-03-04 17:05:27 +0800

    图片请求超过 6 个的部分会等待, 测试 chrome 最多创建 6 个 tcp 链接

---
+++ http-learn/connection/server.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const fs = require('fs')
+
+const conf = {
+    PORT: 3333
+}
+
+const html = fs.readFileSync('./index.html', 'utf8')
+const img = fs.readFileSync('./test.jpg')
+
+http.createServer((request, response) => {
+    const {url} = request
+    console.log(url)
+    if (url === '/') {
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+
+        response.end(html)
+    }
+
+    if (url.startsWith('/test')) {
+        response.writeHead(200, {
+            'Content-Type': 'image/jpg'
+        })
+
+        response.end(img)
+    }
+}).listen(conf.PORT, () => {
+    console.log(`the server is running on ${conf.PORT}`)
+})
